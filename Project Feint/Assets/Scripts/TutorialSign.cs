a7ae7d29bc0d0665d7ec10ae5c1441710bcb4fd0
Josh Bonovich <joshua.bonovich@tkdtrio.com>
2022-11-03 11:24:16 -0500

    Tutorial Signs done

--- Project Feint/Assets/Scripts/TutorialSign.cs
+++ Project Feint/Assets/Scripts/TutorialSign.cs
@@ -1,19 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialSign : MonoBehaviour
 {
-    [SerializeField] private List<string> text = new List<string>();
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] List<string> text = new List<string>();
+    private bool open = false;
+    private int textPos;
+    [SerializeField] GameObject textBox;
+    [SerializeField] Text textBoxText;
+    
+
+    void OnContinue()
     {
+        if(textBox.activeInHierarchy)
+        {
+            textPos++;
+            if(textPos >= text.Count)
+            {
+                textPos = 0;
+                textBox.SetActive(false);
+            }
+            textBoxText.text = text[textPos];
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            textPos = 0;
+            textBoxText.text = text[textPos];
+            textBox.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            textPos = 0;
+            textBoxText.text = text[textPos];
+            textBox.SetActive(false);
+        }
     }
 }
