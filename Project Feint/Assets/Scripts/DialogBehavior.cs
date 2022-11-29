8680f7f3259f6176b7be1a224d8727197c1e8a8c
Josh Bonovich <joshua.bonovich@tkdtrio.com>
2022-11-29 14:06:42 -0600

    Update DialogBehavior.cs

--- Project Feint/Assets/Scripts/DialogBehavior.cs
+++ Project Feint/Assets/Scripts/DialogBehavior.cs
@@ -3,25 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+
+//This script slowly types out an array of dialog with multiple colors to depict multiple people
+//If button(s) are pressed it will skip to the next line or go to the next scene if the dialog is complete
 public class DialogBehavior : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI textDisplay;
     [SerializeField] float delay;
     [SerializeField] TextObject[] dialog;
     [SerializeField] string nextScene;
-    private string[] currentLoggedText;
-    private string currentColor;
     private int current = 0;
+    private string[] loggedText;
+    private string currentColor;
     private string currentText;
     // Start is called before the first frame update
+    //Load the first string and begin typing it out automatically using a corroutine
     void Start()
     {
-        currentLoggedText = new string[dialog.Length];
+        loggedText = new string[dialog.Length];
         currentText = dialog[current].text;
         currentColor = "#" + ColorUtility.ToHtmlStringRGBA(dialog[current].color);
         StartCoroutine(Scroll());
     }
-
+    //Increments current to keep track of what dialog line we are dealing with, and if there is more dialog we update the necessary private variables and run the corroutine again
     void Next()
     {
         current++;
@@ -33,27 +37,29 @@ public class DialogBehavior : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+    //Will update the text box with all of the dialog options, adding a new line after each "line" of dialog
     void UpdateText()
     {
-        textDisplay.text = currentLoggedText[0];
-        for (int i = 1; i<currentLoggedText.Length; i++)
+        textDisplay.text = loggedText[0];
+        for (int i = 1; i< loggedText.Length; i++)
         {
-            textDisplay.text += "\n" + currentLoggedText[i];
+            textDisplay.text += "\n" + loggedText[i];
         }
     }
 
+    //Will slowly scroll through the current text, calling UpdateText every time it does, and when it is over it will then call Next
     IEnumerator Scroll()
     {
         for(int s = 0; s<currentText.Length+1; s++)
         {
-            currentLoggedText[current] = "<color=" + currentColor + ">" + currentText.Substring(0, s) + "</color>";
+            loggedText[current] = "<color=" + currentColor + ">" + currentText.Substring(0, s) + "</color>";
             UpdateText();
             yield return new WaitForSeconds(delay);
         }
         Next();
     }
 
+    //Using the new Unity Input System this script will allow you to either skip to the next line of the dialog or go to the next scene if the dialog is finished
     public void OnSelect()
     {
         StopAllCoroutines();
@@ -63,7 +69,7 @@ public class DialogBehavior : MonoBehaviour
         }
         else
         {
-            currentLoggedText[current] = "<color="+currentColor+">"+currentText+"</color>";
+            loggedText[current] = "<color="+currentColor+">"+currentText+"</color>";
             UpdateText();
             Next();
         }
